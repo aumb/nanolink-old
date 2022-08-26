e76c038ccfc573ceb9be927d24e923ed95b4e6c4
Mathiew Abbas <mathiew95@gmail.com>
2022-08-25 21:01:45 -0500

    refactor: added user usercases exporter

--- nanolink_api/src/features/user/middlewares/user_data_source_injector_middleware.dart
+++ nanolink_api/src/features/user/middlewares/user_data_source_injector_middleware.dart
@@ -3,7 +3,7 @@ import 'package:nanolink_core/nanolink_core.dart';
 
 import '../../../../routes/_middleware.dart';
 import '../data_sources/user_data_source.dart';
-import '../use_cases/get_current_user_use_case.dart';
+import '../use_cases/user_use_cases.dart';
 
 final _getCurrentUserUseCase = GetCurrentUserUseCase(dbClient);
 
